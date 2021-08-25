e030321acfef1507d1d43cf0040147cda7a1a53e
Kimaru thagana <macbuntu@192.168.1.5>
2021-08-25 14:15:53 +0300

    airflow scripts

---
+++ scripts_airflow/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Setup DB Connection String
+AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
+export AIRFLOW__CORE__SQL_ALCHEMY_CONN
+
+AIRFLOW__WEBSERVER__SECRET_KEY="openssl rand -hex 30"
+export AIRFLOW__WEBSERVER__SECRET_KEY
+
+DBT_POSTGRESQL_CONN="postgresql+psycopg2://${DBT_POSTGRES_USER}:${DBT_POSTGRES_PASSWORD}@${DBT_POSTGRES_HOST}:${POSTGRES_PORT}/${DBT_POSTGRES_DB}"
+
+cd /dbt && dbt seed
+rm -f /airflow/airflow-webserver.pid
+
+sleep 10
+airflow upgradedb
+sleep 10
+airflow connections --add --conn_id 'dbt_postgres_instance_raw_data' --conn_uri $DBT_POSTGRESQL_CONN
+airflow scheduler & airflow webserver
